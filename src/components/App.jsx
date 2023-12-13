f329c5c96791dd024d460c38faf2b4de54716848
ImAbhiSharma <abhi55555as25@gmail.com>
2023-12-13 17:04:55 +0530

    Created

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Login from "./Login";
+
+
+var userIsRegistered = true;
+
+
+
+function App() {
+  return (
+    <div className="container">
+      <Login isRegistered={userIsRegistered} />
+    </div>
+  );
+}
+
+export default App;
